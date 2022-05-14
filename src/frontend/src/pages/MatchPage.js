3c4b7ca4dbc52116736d6e1c850914539aad4c25
AkhilOmen <akhilbv333@gmail.com>
2022-05-14 07:55:28 +0530

    Added Upto Match API

---
+++ src/frontend/src/pages/MatchPage.js
@@ -0,0 +1,12 @@
+
+
+
+export const MatchPage = () => {
+    
+    return (
+        <div className="MatchPage">
+            <h1>Match Page</h1>
+            
+        </div>
+    )
+}
--- src/main/java/io/myprojectone/ipl_dashboard/controller/TeamController.java
+++ src/main/java/io/myprojectone/ipl_dashboard/controller/TeamController.java
@@ -1,10 +1,15 @@
 package io.myprojectone.ipl_dashboard.controller;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.myprojectone.ipl_dashboard.model.Match;
 import io.myprojectone.ipl_dashboard.model.Team;
 import io.myprojectone.ipl_dashboard.repository.MatchRepository;
 import io.myprojectone.ipl_dashboard.repository.TeamRepository;
@@ -26,6 +31,13 @@ public class TeamController {
         Team team = this.teamRepository.findByTeamName(teamName);
         team.setMatches(this.matchRepository.FindTheLastestMatchesByTeam(teamName, 4));
         return team;
+    }
+
+    @GetMapping("/team/{teamName}/matches")
+    public List<Match> getMatchesForTeam(@PathVariable String teamName, @RequestParam int year){
+        LocalDate startDate = LocalDate.of(year, 1, 1);
+        LocalDate endDate = LocalDate.of(year + 1, 1, 1);
+        return this.matchRepository.getMatchesByTeamBetweenDates(teamName, startDate, endDate);
     }
 
 }
